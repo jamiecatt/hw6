82bcaacb39b25ccb2e4f9efb7c5b486efdf5fb2a
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-02-27 22:50:25 -0800

    Easing Mouse
    Homework 3 - Challenge

--- challenge.js
+++ challenge.js
@@ -1,1 +1,25 @@
+var x = 0;
+var y = 0;
+var targetx = 5;
+var targety = 5;
 
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+	background(220);
+	var rate = 0.25;
+
+	x = targetx * rate + x * (1 - rate);
+	y = targety * rate + y * (1 - rate);
+
+	rect(x, y, 5, 20);
+  fill(255);
+  noStroke();
+}
+
+function mouseMoved() {
+	targetx = mouseX;
+	targety = mouseY;
+}
